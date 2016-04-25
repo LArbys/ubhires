1361bbdd534339dd5a33220c99b2598c48d0c8aa
Taritree Wongjirad <taritree@nunet.lns.mit.edu>
2016-04-25 18:24:05 -0400

    save network building scripts and network

--- build_trimese_resnet.py
+++ build_trimese_resnet.py
@@ -6,7 +6,7 @@ from caffe import layers as L
 
 augment_data = True
 use_batch_norm = True
-use_dropout = True
+use_dropout = False
 
 def buildnet( inputdb, mean_file, batch_size, height, width, nchannels, net_type="train"):
     net = caffe.NetSpec()
@@ -19,17 +19,17 @@ def buildnet( inputdb, mean_file, batch_size, height, width, nchannels, net_type
     if net_type=="train":
         train = True
 
-    data_layers,label = lt.data_layer_trimese( net, inputdb, mean_file, batch_size, net_type, height, width, nchannels, [4,8], crop_size=768 )
+    data_layers,label = lt.data_layer_trimese( net, inputdb, mean_file, batch_size, net_type, height, width, nchannels, [1,2], crop_size=768 )
 
     # First conv  layer
     branch_ends = []
     for n,layer in enumerate(data_layers):
-        conv1 = lt.convolution_layer( net, layer, "plane%d_conv1"%(n), "tri_conv1", 32, 2, 7, 3, 0.05, addbatchnorm=True, train=train )
+        conv1 = lt.convolution_layer( net, layer, "plane%d_conv1"%(n), "tri_conv1_plane%d"%(n), 64, 2, 5, 3, 0.05, addbatchnorm=True, train=train )
         pool1 = lt.pool_layer( net, conv1, "plane%d_pool1"%(n), 3, 1 )
 
-        conv2 = lt.convolution_layer( net, pool1, "plane%d_conv2"%(n), "tri_conv2", 16, 2, 3, 3, 0.05, addbatchnorm=True, train=train )
+        conv2 = lt.convolution_layer( net, pool1, "plane%d_conv2"%(n), "tri_conv2_plane%d"%(n), 64, 2, 3, 3, 0.05, addbatchnorm=True, train=train )
         
-        conv3 = lt.convolution_layer( net, conv2, "plane%d_conv3"%(n), "tri_conv3", 16, 2, 3, 3, 0.05, addbatchnorm=True, train=train )
+        conv3 = lt.convolution_layer( net, conv2, "plane%d_conv3"%(n), "tri_conv3_plane%d"%(n), 64, 2, 3, 3, 0.05, addbatchnorm=True, train=train )
 
         pool3 = lt.pool_layer( net, conv3, "plane%d_pool3"%(n), 3, 1 )
 
@@ -38,19 +38,19 @@ def buildnet( inputdb, mean_file, batch_size, height, width, nchannels, net_type
     concat = lt.concat_layer( net, "mergeplanes", *branch_ends )
 
 
-    resnet1  = lt.resnet_module( net, concat,  "resnet1", 16*3, 3, 1, 1,8,16, use_batch_norm, train)
-    resnet2  = lt.resnet_module( net, resnet1, "resnet2", 16, 3, 1, 1,8,16, use_batch_norm, train)
-    resnet3  = lt.resnet_module( net, resnet2, "resnet3", 16, 3, 1, 1,8,32, use_batch_norm, train)
+    resnet1  = lt.resnet_module( net, concat,  "resnet1", 64*3, 3, 1, 1,8,32, use_batch_norm, train)
+    resnet2  = lt.resnet_module( net, resnet1, "resnet2", 32, 3, 1, 1,8,32, use_batch_norm, train)
+    resnet3  = lt.resnet_module( net, resnet2, "resnet3", 32, 3, 1, 1,16,64, use_batch_norm, train)
     
-    resnet4  = lt.resnet_module( net, resnet3, "resnet4", 32, 3, 1, 1,8,32, use_batch_norm, train)
-    resnet5  = lt.resnet_module( net, resnet4, "resnet5", 32, 3, 1, 1,8,32, use_batch_norm, train)
-    resnet6  = lt.resnet_module( net, resnet5, "resnet6", 32, 3, 1, 1,16,64, use_batch_norm, train)
+    resnet4  = lt.resnet_module( net, resnet3, "resnet4", 64, 3, 1, 1,16,64, use_batch_norm, train)
+    resnet5  = lt.resnet_module( net, resnet4, "resnet5", 64, 3, 1, 1,16,64, use_batch_norm, train)
+    resnet6  = lt.resnet_module( net, resnet5, "resnet6", 64, 3, 1, 1,32,128, use_batch_norm, train)
 
-    resnet7  = lt.resnet_module( net, resnet6, "resnet7", 64,  3, 1, 1,16,64, use_batch_norm, train)
-    resnet8  = lt.resnet_module( net, resnet7, "resnet8", 64, 3, 1, 1, 16,64, use_batch_norm, train)
-    resnet9  = lt.resnet_module( net, resnet8, "resnet9", 64, 3, 1, 1,32,128, use_batch_norm, train)
+    resnet7  = lt.resnet_module( net, resnet6, "resnet7", 128, 3, 1, 1, 32,128, use_batch_norm, train)
+    resnet8  = lt.resnet_module( net, resnet7, "resnet8", 128, 3, 1, 1, 32,128, use_batch_norm, train)
+    resnet9  = lt.resnet_module( net, resnet8, "resnet9", 128, 3, 1, 1, 64,256, use_batch_norm, train)
         
-    net.lastpool = lt.pool_layer( net, resnet9, "lastpool", 7, 1, P.Pooling.AVE )
+    net.lastpool = lt.pool_layer( net, resnet9, "lastpool", 5, 1, P.Pooling.AVE )
     lastpool_layer = net.lastpool
     
     if use_dropout:
@@ -59,7 +59,7 @@ def buildnet( inputdb, mean_file, batch_size, height, width, nchannels, net_type
                                          dropout_param=dict(dropout_ratio=0.5))
         lastpool_layer = net.lastpool_dropout
     
-    fc1 = lt.final_fully_connect( net, lastpool_layer, nclasses=256 )
+    fc1 = lt.final_fully_connect( net, lastpool_layer, nclasses=512 )
     fc2 = lt.final_fully_connect( net, fc1, nclasses=4096 )
     fc3 = lt.final_fully_connect( net, fc2, nclasses=2 )
     
@@ -72,16 +72,74 @@ def buildnet( inputdb, mean_file, batch_size, height, width, nchannels, net_type
 
     return net
 
+def append_rootdata_layer( prototxt ):
+    fin = open(prototxt,'r')
+    fout = open( prototxt.replace(".prototxt","_rootdata.prototxt"), 'w' )
+    lines = fin.readlines()
+    lout = []
+    found_end_of_data = False
+    mean_file = ""
+    flist = ""
+    for l in lines:
+        #l = l.strip()
+        if found_end_of_data:
+            lout.append(l)
+        if l=="}\n":
+            found_end_of_data = True
+        n = len(l.strip().split(":"))
+        if n>=2 and l.strip().split(":")[0]=="mean_file":
+            mean_file = l.strip().split(":")[1].strip()
+        if n>=2 and l.strip().split(":")[0]=="source":
+            flist = l.strip().split(":")[1].strip()
+        if n>=2 and l.strip().split(":")[0]=="batch_size":
+            batch_size = int(l.strip().split(":")[1].strip())
+    print mean_file,flist,batch_size
+
+    rootlayer = """
+layer {
+  name: "data"
+  type: "ROOTData"
+  top: "data"
+  top: "label"
+
+  root_data_param {
+    source: %s
+    mean: %s
+    mean_producer: "mean"
+    image_producer: "tpc_hires_crop"
+    roi_producer: "tpc_hires_crop"
+    nentries: %d
+    batch_size: %d
+    imin_plane0: 30
+    imin_plane1: 28
+    imin_plane2: 40
+    imax_plane0: 400
+    imax_plane1: 400
+    imax_plane2: 400
+    ch0_mean: 0
+    ch1_mean: 0
+    ch2_mean: 0
+  }    
+}
+""" % (flist,mean_file,batch_size,batch_size)
+
+    print >>fout,rootlayer
+
+    for l in lout:
+        print>>fout,l,
+    fin.close()
+    fout.close()
+
 if __name__ == "__main__":
     
-    traindb    = "/mnt/raid0/taritree/test_data/ccqe_supported_images_train.db"
-    train_mean = "/mnt/raid0/taritree/test_data/ccqe_supported_images_train_mean.bin"
-    testdb     = "/mnt/raid0/taritree/test_data/ccqe_supported_images_test.db"
-    test_mean  = "/mnt/raid0/taritree/test_data/ccqe_supported_images_test_mean.bin"
+    traindb    = "/home/taritree/working/larbys/ubhires/flist_train.txt"
+    train_mean = "/mnt/disk0/kterao/hires_train/train_mean.root"
+    testdb     = "/home/taritree/working/larbys/ubhires/flist_test.txt"
+    test_mean  = "/mnt/disk0/kterao/hires_train/val_mean.root"
     
 
-    train_net = buildnet( traindb, train_mean, 8, 768, 768, 3, net_type="train"  )
-    test_net  = buildnet( testdb,   test_mean, 1, 768, 768, 3, net_type="test"  )
+    train_net   = buildnet( traindb, train_mean, 36, 768, 768, 3, net_type="train"  )
+    test_net    = buildnet( testdb,   test_mean, 1, 768, 768, 3, net_type="test"  )
     deploy_net  = buildnet( testdb, test_mean, 1, 768, 768, 3, net_type="deploy"  )
 
     testout   = open('ub_trimese_resnet_test.prototxt','w')
@@ -93,6 +151,12 @@ if __name__ == "__main__":
     testout.close()
     trainout.close()
     deployout.close()
+
+    append_rootdata_layer( 'ub_trimese_resnet_train.prototxt' )
+    append_rootdata_layer( 'ub_trimese_resnet_test.prototxt' )
+
+    os.system("rm ub_trimese_resnet_train.prototxt")
+    os.system("rm ub_trimese_resnet_test.prototxt")
 
 
 
